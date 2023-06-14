4f959ac5baf6a7cf98becaf3599624c3831c136b
Zaquir <108782708+MainaZaquir@users.noreply.github.com>
2023-06-14 16:19:54 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,11 +7,11 @@ def evaluate_expression():
     except:
         result_label.config(text="Invalid expression")
 
-# Create the main window
+# Create the main window for  your calculator
 window = tk.Tk()
 window.title("Calculator")
 
-# Create the UI elements
+# Create the correct UI elements for your calculaltor
 entry = tk.Entry(window, width=30)
 entry.pack(pady=10)
 
@@ -21,5 +21,5 @@ evaluate_button.pack()
 result_label = tk.Label(window, text="Result: ")
 result_label.pack(pady=10)
 
-# Start the main loop
+# Start the main loop of your calculator
 window.mainloop()
